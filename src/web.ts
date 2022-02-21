d3aa107dfa013f6372d4aefb286bfd5883e9967f
Sergey Chugaev <elov4anin@gmail.com>
2022-02-21 16:40:18 +0500

    add info to README.md

--- src/web.ts
+++ src/web.ts
@@ -5,6 +5,7 @@ import type { VerYaLocatorPlugin } from './definitions';
 export class VerYaLocatorWeb extends WebPlugin implements VerYaLocatorPlugin {
 
   async requestCoordinates(options: { version: string, url: string, api_key: string }): Promise<{ version: string, url: string, api_key: string }> {
+    console.log('options', options);
     throw this.unimplemented('Not implemented on web.');
   }
 }
