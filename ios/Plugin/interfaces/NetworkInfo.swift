de5d1f04511bcd9286f956f3765d66f326511612
Sergey Chugaev <elov4anin@gmail.com>
2022-02-21 14:16:42 +0500

    ios

---
+++ ios/Plugin/interfaces/NetworkInfo.swift
@@ -0,0 +1,13 @@
+//
+//  networkInfo.swift
+//  Plugin
+//
+//  Created by Сергей Чугаев on 18.02.2022.
+//  Copyright © 2022 Max Lynch. All rights reserved.
+//
+
+import Foundation
+struct NetworkInfo {
+    gsm_cells: String
+    wifi_networks: String
+}
